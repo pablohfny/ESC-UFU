7cf6b42705e1a0deccedd0e57f5aa29e75f6ae95
Pablo Nunes <admin@walleautomacao.com.br>
2023-03-16 17:43:54 -0300

    Implemented test for gtkwave

--- Exemplos/Aula 01/half-adder/half-adder.v
+++ Exemplos/Aula 01/half-adder/half-adder.v
@@ -5,3 +5,30 @@ module halfadder(a,b,sum,carry);
   xor xor_1(s,a,b);
   and and_1(c,a,b);
 endmodule
+
+
+module test_halfadder;
+
+  reg a,b;
+  wire sum,carry;
+
+  halfadder ha(a,b,sum,carry);
+
+  initial begin
+    $dumpfile("halfadder.vcd");
+    $dumpvars(0,test_halfadder);
+    a = 0;
+    b = 0;
+    #10;
+    a = 0;
+    b = 1;
+    #10;
+    a = 1;
+    b = 0;
+    #10;
+    a = 1;
+    b = 1;
+    #10;
+    $finish;
+  end
+endmodule;
